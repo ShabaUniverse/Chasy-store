821c3a41dad6549e0e778d65e44170ff5b3841ab
shabdan <kozhonalievshabdan@gmail.com>
2024-01-21 00:42:36 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "../src/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="logo">
+        <h1 className="check">CHASY</h1>
+        <p>on your wrist</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
