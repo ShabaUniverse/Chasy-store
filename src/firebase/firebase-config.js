67396cf669819d484e9a53a6c8dccdd156830f0f
shabdan <kozhonalievshabdan@gmail.com>
2024-01-30 12:44:44 +0600

    installed firebase

---
+++ src/firebase/firebase-config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA-D1PLF4l3wMzA7_ivA9B8DtY6EcnwtNE",
+  authDomain: "chasy-2049a.firebaseapp.com",
+  projectId: "chasy-2049a",
+  storageBucket: "chasy-2049a.appspot.com",
+  messagingSenderId: "498111888925",
+  appId: "1:498111888925:web:1eee808191f96f1d32d0f4"
+};
+
+
+const app = initializeApp(firebaseConfig);
