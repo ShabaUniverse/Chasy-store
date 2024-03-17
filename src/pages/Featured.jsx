95758d50f904583502b59f80722d89c4af3e29da
shabdan <kozhonalievshabdan@gmail.com>
2024-03-17 23:06:00 +0600

    compplete page for featured

--- src/pages/Featured.jsx
+++ src/pages/Featured.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import '../styles/pages/Featured.scss';
+import {doc, getDoc} from "firebase/firestore";
+import {db} from '../firebase/firebase-config';
 
 const Featured = () => {
+
+  const [featuredProducts, setFeaturedProducts] = useState([]);
+
+  useEffect(() => {
+    const getDataFromFirestore = async () => {
+      const docRef = doc(db, 'products', "allPRoDucTsqxshdNshrZ");
+      const docSnap = await getDoc(docRef);
+      const snapshot = docSnap.data();
+      setFeaturedProducts(snapshot.products.slice(10, 20));
+    }
+    getDataFromFirestore();
+  }, [])
   return (
-    <div>
-      Featured
+    <div className='featured'>
+      <div className="featured-container">
+        <div className="featured-head">
+          Featured Products
+        </div>
+
+        <div className="featured-products">
+          {
+            featuredProducts.map((item, index) => (
+              <div className="product">
+                <img src={item.image} alt="" />
+                <p>{item.title}</p>
+              </div>
+            ))
+          }
+        </div>
+      </div>
     </div>
   )
 }
