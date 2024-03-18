ec336487f12b144d6f2d2ba71c94ef00f365ce20
shabdan <kozhonalievshabdan@gmail.com>
2024-03-18 14:07:13 +0600

    pending was added

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -6,14 +6,18 @@ import { doc, getDoc } from "firebase/firestore";
 const Shop = () => {
 
   const [shopProducts, setShopProducts] = useState([]);
+  const [pending, setPending] = useState(false);
+
 
   useEffect(() => {
     const getDataFromFirestore = async () => {
+      setPending(true);
       const docRef = doc(db, "products", "allPRoDucTsqxshdNshrZ");
       const docSnap = await getDoc(docRef);
       const snapshot = docSnap.data();
       console.log(snapshot.products);
       setShopProducts(snapshot.products);
+      setPending(false);
     };
     getDataFromFirestore();
   }, []);
@@ -22,11 +26,12 @@ const Shop = () => {
       <div className="shop">
         <div className="shop-container">
             <div className="shop-head">
-                <h3> Chasy Shop </h3>
+                {!pending && <p>Chasy Shop</p>} 
             </div>
+
             <div className="shop-products">
                 {
-                    shopProducts.map((item, index) => (
+                    pending === true ? <span>Loading Data</span> : shopProducts.map((item, index) => (
                         <div className="product" key={index}>
                             <img src={item.image} alt="" />
                             <p>{item.title}</p>
